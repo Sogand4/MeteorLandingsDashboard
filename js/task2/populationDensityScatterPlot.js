2218e6a9088e267dda330cbe6143204ee9bdc4eb
rbajaj <rbajaj@students.cs.ubc.ca>
2026-03-31 10:36:10 -0700

    included powers of 10 on the axes

--- js/task2/populationDensityScatterPlot.js
+++ js/task2/populationDensityScatterPlot.js
@@ -3,10 +3,8 @@ class PopulationDensityScatterPlot {
     this.config = {
       parentElement: _config.parentElement,
       containerWidth: _config.containerWidth || 700,
-      containerHeight: _config.containerHeight || 460,
-      margin: {
-        top: 60, right: 25, bottom: 65, left: 95,
-      },
+      containerHeight: _config.containerHeight || 300,
+      margin: { top: 60, right: 25, bottom: 65, left: 95 }
     };
     this.data = data;
     this.initVis();
@@ -15,82 +13,84 @@ class PopulationDensityScatterPlot {
   initVis() {
     const vis = this;
 
-    vis.width = vis.config.containerWidth
-      - vis.config.margin.left
-      - vis.config.margin.right;
+    vis.width =
+      vis.config.containerWidth -
+      vis.config.margin.left -
+      vis.config.margin.right;
 
-    vis.height = vis.config.containerHeight
-      - vis.config.margin.top
-      - vis.config.margin.bottom;
+    vis.height =
+      vis.config.containerHeight -
+      vis.config.margin.top -
+      vis.config.margin.bottom;
 
     vis.svg = d3
       .select(vis.config.parentElement)
-      .append('svg')
-      .attr('width', vis.config.containerWidth)
-      .attr('height', vis.config.containerHeight);
+      .append("svg")
+      .attr("width", vis.config.containerWidth)
+      .attr("height", vis.config.containerHeight);
 
     vis.svg
-      .append('defs')
-      .append('clipPath')
-      .attr('id', 'scatter-clip')
-      .append('rect')
-      .attr('width', vis.width)
-      .attr('height', vis.height);
+      .append("defs")
+      .append("clipPath")
+      .attr("id", "scatter-clip")
+      .append("rect")
+      .attr("width", vis.width)
+      .attr("height", vis.height);
 
     vis.chart = vis.svg
-      .append('g')
+      .append("g")
       .attr(
-        'transform',
-        `translate(${vis.config.margin.left},${vis.config.margin.top})`,
+        "transform",
+        `translate(${vis.config.margin.left},${vis.config.margin.top})`
       );
 
     vis.plotArea = vis.chart
-      .append('g')
-      .attr('clip-path', 'url(#scatter-clip)');
+      .append("g")
+      .attr("clip-path", "url(#scatter-clip)");
 
     vis.svg
-      .append('text')
-      .attr('class', 'chart-title')
-      .attr('x', vis.config.containerWidth / 2)
-      .attr('y', 30)
-      .attr('text-anchor', 'middle')
-      .text('Meteorite Landings vs. Population Density');
+      .append("text")
+      .attr("class", "chart-title")
+      .attr("x", vis.config.containerWidth / 2)
+      .attr("y", 30)
+      .attr("text-anchor", "middle")
+      .text("Meteorite Landings vs. Population Density");
 
     vis.xAxisGroup = vis.chart
-      .append('g')
-      .attr('class', 'axis axis-x')
-      .attr('transform', `translate(0, ${vis.height})`);
+      .append("g")
+      .attr("class", "axis axis-x")
+      .attr("transform", `translate(0, ${vis.height})`);
 
     vis.yAxisGroup = vis.chart
-      .append('g')
-      .attr('class', 'axis axis-y');
+      .append("g")
+      .attr("class", "axis axis-y");
 
     vis.xGridGroup = vis.chart
-      .append('g')
-      .attr('class', 'grid grid-x')
-      .attr('transform', `translate(0, ${vis.height})`);
+      .append("g")
+      .attr("class", "grid grid-x")
+      .attr("transform", `translate(0, ${vis.height})`);
 
     vis.yGridGroup = vis.chart
-      .append('g')
-      .attr('class', 'grid grid-y');
+      .append("g")
+      .attr("class", "grid grid-y");
 
     vis.svg
-      .append('text')
-      .attr('class', 'axis-label')
-      .attr('x', vis.config.containerWidth / 2)
-      .attr('y', vis.config.containerHeight - 12)
-      .attr('text-anchor', 'middle')
-      .text('Number of Meteor Landings (log scale)');
+      .append("text")
+      .attr("class", "axis-label")
+      .attr("x", vis.config.containerWidth / 2)
+      .attr("y", vis.config.containerHeight - 12)
+      .attr("text-anchor", "middle")
+      .text("Number of Meteor Landings (log scale)");
 
     vis.svg
-      .append('text')
-      .attr('class', 'axis-label')
+      .append("text")
+      .attr("class", "axis-label")
       .attr(
-        'transform',
-        `translate(24, ${vis.config.containerHeight / 2}) rotate(-90)`,
+        "transform",
+        `translate(24, ${vis.config.containerHeight / 2}) rotate(-90)`
       )
-      .attr('text-anchor', 'middle')
-      .text('Population Density (log scale)');
+      .attr("text-anchor", "middle")
+      .text("Population Density (log scale)");
   }
 
   wrangleData() {
@@ -99,51 +99,52 @@ class PopulationDensityScatterPlot {
     vis.chartData = d3
       .rollups(
         vis.data.filter(
-          (d) => d.country
-            && d.population_density != null
-            && !Number.isNaN(+d.population_density),
+          d =>
+            d.country &&
+            d.population_density != null &&
+            !isNaN(+d.population_density)
         ),
-        (values) => ({
+        values => ({
           landings: values.length,
-          population_density: +values[0].population_density,
+          population_density: +values[0].population_density
         }),
-        (d) => d.country,
+        d => d.country
       )
       .map(([country, values]) => ({
         country,
         landings: +values.landings,
-        population_density: +values.population_density,
+        population_density: +values.population_density
       }))
       .filter(
-        (d) => !Number.isNaN(d.landings)
-          && !Number.isNaN(d.population_density)
-          && d.landings >= 1
-          && d.population_density >= 1,
+        d =>
+          !isNaN(d.landings) &&
+          !isNaN(d.population_density) &&
+          d.landings >= 1 &&
+          d.population_density >= 1
       )
       .sort((a, b) => a.landings - b.landings);
   }
 
-  static buildYTicks(yMax) {
-    const baseTicks = [1, 2, 5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000]
-      .filter((d) => d <= yMax);
+  buildPowerOfTenTicks(maxValue) {
+    const ticks = [];
+    let power = 0;
 
-    if (yMax <= 5000) {
-      return baseTicks;
+    while (10 ** power <= maxValue) {
+      ticks.push(10 ** power);
+      power += 1;
     }
 
-    const extraTicks = [];
-    const upperTickCount = 3; // number of proportional ticks from 5k to max
+    return ticks;
+  }
 
-    const log5000 = Math.log10(5000);
-    const logMax = Math.log10(yMax);
+  buildTicksWithMax(baseTicks, maxValue) {
+    const ticks = [...baseTicks];
 
-    for (let i = 1; i <= upperTickCount; i++) {
-      const t = i / upperTickCount;
-      const value = 10 ** (log5000 + (logMax - log5000) * t);
-      extraTicks.push(Math.round(value));
+    if (!ticks.includes(maxValue)) {
+      ticks.push(maxValue);
     }
 
-    return [...new Set([...baseTicks, ...extraTicks])].sort((a, b) => a - b);
+    return [...new Set(ticks)].sort((a, b) => a - b);
   }
 
   updateVis() {
@@ -152,8 +153,8 @@ class PopulationDensityScatterPlot {
 
     if (!vis.chartData.length) return;
 
-    const xMax = d3.max(vis.chartData, (d) => d.landings);
-    const yMax = d3.max(vis.chartData, (d) => d.population_density);
+    const xMax = d3.max(vis.chartData, d => d.landings);
+    const yMax = d3.max(vis.chartData, d => d.population_density);
 
     if (!(xMax > 0) || !(yMax > 0)) return;
 
@@ -167,11 +168,14 @@ class PopulationDensityScatterPlot {
       .domain([1, yMax])
       .range([vis.height, 0]);
 
-    vis.xTicks = [1, 2, 5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000]
-      .filter((d) => d >= 1 && d <= xMax);
+    const baseXTicks = [1, 2, 5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000]
+      .filter(d => d >= 1 && d <= xMax);
 
-    vis.yGridTicks = vis.buildYTicks(yMax);
-    vis.yAxisTicks = vis.buildYTicks(yMax);
+    const baseYTicks = vis.buildPowerOfTenTicks(yMax);
+
+    vis.xTicks = vis.buildTicksWithMax(baseXTicks, xMax);
+    vis.yGridTicks = vis.buildTicksWithMax(baseYTicks, yMax);
+    vis.yAxisTicks = vis.buildTicksWithMax(baseYTicks, yMax);
 
     vis.renderVis();
   }
@@ -183,52 +187,69 @@ class PopulationDensityScatterPlot {
       d3.axisBottom(vis.xScale)
         .tickValues(vis.xTicks)
         .tickSize(-vis.height)
-        .tickFormat(''),
+        .tickFormat("")
     );
 
     vis.yGridGroup.call(
       d3.axisLeft(vis.yScale)
         .tickValues(vis.yGridTicks)
         .tickSize(-vis.width)
-        .tickFormat(''),
+        .tickFormat("")
     );
 
     vis.xAxisGroup.call(
       d3.axisBottom(vis.xScale)
         .tickValues(vis.xTicks)
-        .tickFormat(d3.format('~s')),
+        .tickFormat(d3.format("~s"))
     );
 
     vis.yAxisGroup.call(
       d3.axisLeft(vis.yScale)
         .tickValues(vis.yAxisTicks)
-        .tickFormat(d3.format('~s')),
+        .tickFormat(d3.format("~s"))
     );
 
-    vis.chart.selectAll('.grid .domain').remove();
+    vis.chart.selectAll(".grid .domain").remove();
 
     vis.yAxisGroup
-      .selectAll('.tick text')
-      .attr('font-size', '10px')
-      .attr('x', -10)
-      .attr('dy', '0.32em')
-      .style('text-anchor', 'end');
+      .selectAll(".tick text")
+      .attr("font-size", "10px")
+      .attr("x", -10)
+      .attr("dy", "0.32em")
+      .style("text-anchor", "end");
+
+    const xTickTexts = vis.xAxisGroup.selectAll(".tick text");
+
+    xTickTexts
+      .attr("font-size", "9px")
+      .attr("dy", "0.9em")
+      .style("text-anchor", "middle");
+
+    const xTicks = vis.xAxisGroup.selectAll(".tick").nodes();
+    if (xTicks.length >= 2) {
+      const secondLastTick = d3.select(xTicks[xTicks.length - 2]).select("text");
+      const lastTick = d3.select(xTicks[xTicks.length - 1]).select("text");
+
+      secondLastTick.attr("dx", "-0.7em");
+      lastTick.attr("dx", "0.7em");
+    }
 
     const points = vis.plotArea
-      .selectAll('.scatter-point')
-      .data(vis.chartData, (d) => d.country)
-      .join('circle')
-      .attr('class', 'scatter-point')
-      .attr('cx', (d) => vis.xScale(d.landings))
-      .attr('cy', (d) => vis.yScale(d.population_density))
-      .attr('r', 4);
+      .selectAll(".scatter-point")
+      .data(vis.chartData, d => d.country)
+      .join("circle")
+      .attr("class", "scatter-point")
+      .attr("cx", d => vis.xScale(d.landings))
+      .attr("cy", d => vis.yScale(d.population_density))
+      .attr("r", 4);
 
-    points.selectAll('title').remove();
+    points.selectAll("title").remove();
 
     points
-      .append('title')
+      .append("title")
       .text(
-        (d) => `${d.country}\nLandings: ${d.landings}\nPopulation density: ${d3.format('.1f')(d.population_density)}`,
+        d =>
+          `${d.country}\nLandings: ${d.landings}\nPopulation density: ${d3.format(".1f")(d.population_density)}`
       );
   }
 }
